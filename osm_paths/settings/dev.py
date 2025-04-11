dbe0774baa24d06b671fbc1467418f75ca88ab85
J-E Castagnede <j.e.castagnede@gmail.com>
2025-04-11 10:37:05 +0200

    pep8

--- osm_paths/settings/dev.py
+++ osm_paths/settings/dev.py
@@ -4,4 +4,4 @@ DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-ta38laalyq-bgjh5-$ev%vy$v@h$e-^04cv9$vctnjr)9q#twf"
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
